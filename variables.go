080830119209c353d323f1a56ede885e9bd10f8d
Rajatha <149949532+Rajatha19@users.noreply.github.com>
2025-07-27 12:41:34 +0530

    Add files via upload

---
+++ variables.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+func main(){
+	/*
+	var name string = "John" %s
+	var age int = 25 %d
+	var student bool = true %t
+	var marks float32 = 95.5 %f
+	var initial rune ='J' %c
+
+	var(
+		name="John"
+		age=25
+		student=true
+		marks=95.5
+		initial='J'
+	)
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(student)
+	fmt.Println(marks)
+	fmt.Println(initial)
+    fmt.Printf("%c",initial)
+	*/
+	// assign new variable, constant variable:
+	const age=25
+	var name =" john"
+	lastName := "doe"
+	fmt.Println(name,age,lastName)
+}
